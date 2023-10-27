6938133fc94f4e97430e9651541d6bfbe40a64c7
Lovesomemadara <borovkun@gmail.com>
2023-10-27 15:10:39 +0500

    ДЗ по первой теме, начало работы
    * Задания с 0 по 9ый выполнены.

--- tasks/task_3.py
+++ tasks/task_3.py
@@ -1,7 +1,7 @@
-string_literal = ...  # здесь Ваш код
-integer_literal = ...  # здесь Ваш код
-float_literal = ...  # здесь Ваш код
-boolean_literal = ...  # здесь Ваш код
+string_literal = "Привет, Мир!"  # здесь Ваш код
+integer_literal = 2  # здесь Ваш код
+float_literal = 2.5  # здесь Ваш код
+boolean_literal = True  # здесь Ваш код
 
 print(string_literal)
 print(integer_literal)
